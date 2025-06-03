1ef41f0125278a4f2e567c96a7cecc94b3f9de40
walu-dev <jonathanenoch11@gmail.com>
2025-06-03 15:46:05 +0300

    Calculate simple intrest

--- Task_2/main.c
+++ Task_2/main.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "simpleIntrest.h"
 
 int main()
 {
-    printf("Hello world!\n");
-    return 0;
+    CalculateSimpleIntrest();
 }
--- Task_2/simpleIntrest.c
+++ Task_2/simpleIntrest.c
@@ -1,1 +1,10 @@
+#include <stdio.h>
+#include "simpleIntrest.h"
 
+void CalculateSimpleIntrest(){
+    double principle = 20000;
+    double time  = 5;
+    double rate = 0.1; //10% rate
+    double SimpleIntrest = principle * time * rate;
+    printf("The SimpleIntrest is: %.2lf", SimpleIntrest);
+}
