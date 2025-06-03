156ac5742d0e73ad8c531fa55ac2a7569fbf0c0a
walu-dev <jonathanenoch11@gmail.com>
2025-06-03 18:23:41 +0300

    Calculate currency exchange
    Convert 700usd to ugx

--- Task_2/Currency.c
+++ Task_2/Currency.c
@@ -1,1 +1,9 @@
+#include <stdio.h>
+#include "Currency.h"
 
+void ConvertCurrency(){
+    const double Rate = 3500;// Amount in ugx
+    double Amount = 700;
+    double exchange = Amount*Rate; // Amount in usd
+    printf("The final amount after exchange is: %.2lf UGX\n ", exchange);
+}
--- Task_2/main.c
+++ Task_2/main.c
@@ -2,12 +2,14 @@
 #include <stdlib.h>
 #include "simpleIntrest.h"
 #include "DiscountedPrice.h"
+#include "Currency.h"
 
 
 int main()
 {
     CalculateSimpleIntrest();
     CalculateDiscountPrice();
+    ConvertCurrency();
 
     return 0;
 }
